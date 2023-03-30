f0bf5efd79026c3df033389119aa5a0cfd65c271
Aleks247 <aleks247@abv.bg>
2023-03-30 12:15:34 +0300

    Initial commit

---
+++ src/main/java/com/Aleks247/personalCalendar_V2/PersonalCalendarV2Application.java
@@ -0,0 +1,13 @@
+package com.Aleks247.personalCalendar_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCalendarV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCalendarV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Aleks247/personalCalendar_V2/PersonalCalendarV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aleks247.personalCalendar_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCalendarV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
